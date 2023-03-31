b5e588fa3f352e1152834fa1396f397aa3f56e86
Lucas Gilio Ducci <lucasgducci@hotmail.com>
2023-03-31 06:45:36 -0300

    Implementando Ipersistencia no MysqlDriver

--- Infra/MySqlDriver.cs
+++ Infra/MySqlDriver.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProdutoEstoque.Infra.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,44 @@ using System.Threading.Tasks;
 
 namespace ProdutoEstoque.Infra
 {
-    class MySqlDriver
+    public class MySqlDriver<T> : IPersistencia<T>
     {
+
+        public MySqlDriver(string localGravacao)
+        {
+            this.localGravacao = localGravacao;
+        }
+
+        private string localGravacao = "";
+
+        public string GetLocalGravacao()
+        {
+            return this.localGravacao;
+        }
+
+        public Task<T?> BuscarPorId(string id)
+        {
+            throw new NotImplementedException();
+        }        
+
+        public Task Excluir(T objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ExcluirTudo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Salvar(T objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> Todos()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
