7f6dd06df10199083fb4b6252e41bd4a942d419d
Lucas Gilio Ducci <lucasgducci@hotmail.com>
2023-04-01 14:27:36 -0300

    Implementando conexao com mysql

--- Infra/MySqlDriver.cs
+++ Infra/MySqlDriver.cs
@@ -22,27 +22,27 @@ namespace ProdutoEstoque.Infra
             return this.localGravacao;
         }
 
-        public Task<T?> BuscarPorId(string id)
+        public async Task<T?> BuscarPorId(string id)
         {
             throw new NotImplementedException();
         }        
 
-        public Task Excluir(T objeto)
+        public async Task Excluir(T objeto)
         {
             throw new NotImplementedException();
         }
 
-        public Task ExcluirTudo()
+        public async Task ExcluirTudo()
         {
             throw new NotImplementedException();
         }
 
-        public Task Salvar(T objeto)
+        public async Task Salvar(T objeto)
         {
             throw new NotImplementedException();
         }
 
-        public Task<List<T>> Todos()
+        public async Task<List<T>> Todos()
         {
             throw new NotImplementedException();
         }
